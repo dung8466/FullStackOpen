25560eb52594f4477778d7675071b415291463f9
ntd8466 <ntd8466@gmail.com>
2023-09-07 00:21:55 +0700

    Introduction to React

---
+++ part1/introduction/src/Total.jsx
@@ -0,0 +1,12 @@
+import ReactDOM from 'react-dom/client'
+
+const Total = (props) => {
+  return (
+    <>
+      <p> Number of exercises {props.total} </p>
+    </>
+  )
+}
+
+export default Total
+
